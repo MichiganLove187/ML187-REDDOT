1275234087b11775ca8bc5dc5e5225ad0f833688
MichiganLove187 <147991628+MichiganLove187@users.noreply.github.com>
2025-05-14 10:36:02 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'ML187'
+description 'RED DOT CROSSHAIR.'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/crosshair.png',
+    'ui/index.html',
+    'ui/script.js',
+    'ui/style.css'
+}
+
+lua54 'yes'
